a703b006ba9ded10f06043bb08bdddc06c088782
Jason Platts <plattsjm@icloud.com>
2021-07-27 12:54:07 -0400

    Improved API error checking.

--- Scripts/list.js
+++ Scripts/list.js
@@ -15,40 +15,47 @@ exports.List = class List {
   */
   async loadItems() {
     if (FUNCTIONS.isWorkspace()) {
-      for (const gemName of this._gemNames) {
-        let apiGemData                          = await this.fetchRubyGem(gemName)
+      for (let gemName of this._gemNames) {
+        try {
+          let apiGemData                          = await this.fetchRubyGem(gemName)
 
-        let item                                = new ListItem(gemName)
+          let item                                = new ListItem(gemName)
 
-        item.collapsibleState                   = TreeItemCollapsibleState.Expanded
-        item.image                              = 'sidebar-list-item'
+          item.collapsibleState                   = TreeItemCollapsibleState.Expanded
+          item.image                              = 'sidebar-list-item'
 
-        let subItemInfo                         = new ListItem('Info')
-        subItemInfo.descriptiveText             = apiGemData.info
-        subItemInfo.image                       = 'sidebar-list-sub-item'
-        item.children.push(subItemInfo)
+          let subItemInfo                         = new ListItem('Info')
+          subItemInfo.descriptiveText             = apiGemData.info
+          subItemInfo.image                       = 'sidebar-list-sub-item'
+          item.children.push(subItemInfo)
 
-        let subItemAuthors                      = new ListItem('Authors')
-        subItemAuthors.descriptiveText          = apiGemData.authors
-        subItemAuthors.image                    = 'sidebar-list-sub-item'
-        item.children.push(subItemAuthors)
+          let subItemAuthors                      = new ListItem('Authors')
+          subItemAuthors.descriptiveText          = apiGemData.authors
+          subItemAuthors.image                    = 'sidebar-list-sub-item'
+          item.children.push(subItemAuthors)
 
-        let subItemVersion                      = new ListItem('Latest Version')
-        subItemVersion.descriptiveText          = apiGemData.version
-        subItemVersion.image                    = 'sidebar-list-sub-item'
-        item.children.push(subItemVersion)
+          let subItemVersion                      = new ListItem('Latest Version')
+          subItemVersion.descriptiveText          = apiGemData.version
+          subItemVersion.image                    = 'sidebar-list-sub-item'
+          item.children.push(subItemVersion)
 
-        let subItemVersionDownloads             = new ListItem('Version Downloads')
-        subItemVersionDownloads.descriptiveText = apiGemData.version_downloads.toLocaleString('en-US')
-        subItemVersionDownloads.image           = 'sidebar-list-sub-item'
-        item.children.push(subItemVersionDownloads)
+          let subItemVersionDownloads             = new ListItem('Version Downloads')
+          subItemVersionDownloads.descriptiveText = apiGemData.version_downloads.toLocaleString('en-US')
+          subItemVersionDownloads.image           = 'sidebar-list-sub-item'
+          item.children.push(subItemVersionDownloads)
 
-        let subItemGemURL                       = new ListItem('Gem URL')
-        subItemGemURL.descriptiveText           = apiGemData.gem_uri
-        subItemGemURL.image                     = 'sidebar-list-sub-item'
-        item.children.push(subItemGemURL)
+          let subItemGemURL                       = new ListItem('Gem URL')
+          subItemGemURL.descriptiveText           = apiGemData.gem_uri
+          subItemGemURL.image                     = 'sidebar-list-sub-item'
+          item.children.push(subItemGemURL)
 
-        this._items.push( item )
+          this._items.push( item )
+        } catch(error) {
+          FUNCTIONS.showNotification('RubyGems.org API Error',
+            'There was an error retrieving information for one or more Gems from RubyGems.org. ' +
+            'The site may be down or one or more gems in the Gemfile may not exist. ' +
+            'Please check and try again or submit an extension issue report.')
+        }
       }
     }
 
@@ -60,17 +67,15 @@ exports.List = class List {
   */
   async fetchRubyGem(gemName) {
     return new Promise((resolve, reject) => {
-      // try {
       fetch(`https://rubygems.org/api/v1/gems/${gemName}.json`)
         .then(response => response.json())
         .then(data => {
           resolve(data)
         })
-      // } catch (error) {
-      //   FUNCTIONS.showConsoleError(error)
-      //   reject(error)
-      // }
-
+        .catch(error => {
+          FUNCTIONS.showConsoleError(`${error} -- Gem name: ${gemName}`)
+          reject(error)
+        })
     })
   }
 
